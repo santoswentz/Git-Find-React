165bb09ca01ae21ee128a04bb5362e2ce18f186f
eduardowentz <134004996+santoswentz@users.noreply.github.com>
2025-11-05 16:13:24 -0300

    Add ItemList component and repository section to Home
    Introduces the ItemList component with associated styles for displaying repository items. Updates the Home page to include a repository section, input state management, and a search button handler for future GitHub user data fetching. Also adds related CSS for the new UI elements.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,33 @@
 import backgroundImage from '../../assets/background.png';
 import { Header } from '../../components/Header';
+import { ItemList } from '../../components/ItemList';
 import './styles.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repositories, setRepositories] = useState([]);
+
+  const handleGetData = async () => {
+    // Implement search functionality here
+    const userData = await fetch('https://api.github.com/users/' + user);
+    const newUser = await userData.json();
+
+    console.log(newUser);
+  }
+
   return (
     <div className="App">  
       <Header />
         <div className="content">
+          <aside>
           <img src={backgroundImage} alt="Background" className='background-image' />
+          </aside>
           <div className="text-content">
-            <input type="text" placeholder="Search GitHub repositories..." name='usuario' />
-            <button>Search</button> 
+            <input type="text" placeholder="Search GitHub repositories..." name='usuario' value={user} onChange={(e) => setUser(e.target.value)} /> 
+            <button onClick={handleGetData}>Search</button> 
               <div className="profile-section"> 
                   <img src="https://avatars.githubusercontent.com/u/134004996?s=400&u=3d680ee2b8cb947fc737ff506c491d117f182cf3&v=4" alt="User Avatar"   />
                   <div className="user-details">
@@ -19,6 +36,13 @@ function App() {
                     <p>Dev Full-Stack com 1 ano de experiência</p>
                   </div>
               </div>
+            <div className='repositories-section'>
+              <hr />
+              <h4>Repositories</h4>
+              <ItemList title="Item 1" description="Description for Item 1" />
+              <ItemList title="Item 2" description="Description for Item 2" />
+              <ItemList title="Item 3" description="Description for Item 3" />
+            </div>
             </div>
           </div>
     </div>
@@ -26,12 +50,4 @@ function App() {
 }
 
 export default App;
-
-          {/* </div>
-          <div className="repositories-section">
-            <div className="repository-list">
-              <div className="repository-item">
-                <h2>Repository Name</h2>
-              </div>
-            </div>
-        </div> */}
+ 
